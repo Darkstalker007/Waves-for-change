c9831bdf6baf4589de1c3efe30a425c9773d1a25
Satvik Singh <satviksingh835@example.com>
2025-02-22 23:09:15 +0400

    Added files

--- app.py
+++ app.py
@@ -1,9 +1,33 @@
 from flask import Flask,render_template
 
+DRIVES=[
+  {
+    'id':1,
+    'destination':'Mamzar Beach',
+    'duration': 2,
+    'time':'10:00 AM',
+    'active':True,
+  },
+  {
+    'id':2,
+    'destination':'Al Khan Beach',
+    'duration': 2.5,
+    'time':'11:00 AM',
+    'active':True,
+  },
+  {
+    'id':1,
+    'destination':'Kite Beach',
+    'duration': 3,
+    'time':'12:00 PM',
+    'active':False,
+  },
+]
+
 app=Flask(__name__)
 @app.route("/")
 def hello():
-  return render_template('home.html')
+  return render_template('home.html',drives=DRIVES)
 
 if __name__=="__main__":
-  app.run(host="0.0.0.0",debug=True)
+  app.run(host="0.0.0.0",debug=True, port=2000)
