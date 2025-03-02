41661e0698b3aa712708aa527ffa817febf035c8
Satvik Singh <satviksingh835@example.com>
2025-03-02 13:34:41 +0400

    Add error handling for database connection in load_drives function; remove unused models.py

--- database.py
+++ database.py
@@ -1,7 +1,7 @@
 from dotenv import load_dotenv # type: ignore
 import os
 from sqlalchemy import create_engine, text
-
+from sqlalchemy.exc import OperationalError
 
 load_dotenv("connection.env")
 
@@ -11,11 +11,14 @@ if not DATABASE_URL:
 
 engine = create_engine(DATABASE_URL)
 
-
 def load_drives():
-    with engine.connect() as conn:
-        result = conn.execute(text("SELECT * FROM drives"))
-        drives = []
-        for row in result:
-            drives.append(dict(row._mapping))
-        return drives
+    try:
+        with engine.connect() as conn:
+            result = conn.execute(text("SELECT * FROM drives"))
+            drives = []
+            for row in result:
+                drives.append(dict(row._mapping))
+            return drives
+    except OperationalError as e:
+        print(f"Error connecting to the database: {e}")
+        return []
